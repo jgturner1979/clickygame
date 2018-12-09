a2ccfd30edf12ea9be1ee1ff1ddcd3918d8ad84a
Greg Turner <jgregoryturner@gmail.com>
2018-12-08 17:39:42 -0700

    Updates

---
+++ precache-manifest.39b9da85b6107af09856e233874baeb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89ee2dced54b4a3a1599f4b1bf8f4dd8",
+    "url": "/clickygame/static/media/background.89ee2dce.jpg"
+  },
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "03db9ad5aef4e8d5f2bd",
+    "url": "/clickygame/static/js/main.03db9ad5.chunk.js"
+  },
+  {
+    "revision": "39d23f59c0e644bbb6ee",
+    "url": "/clickygame/static/js/1.39d23f59.chunk.js"
+  },
+  {
+    "revision": "03db9ad5aef4e8d5f2bd",
+    "url": "/clickygame/static/css/main.891cee27.chunk.css"
+  },
+  {
+    "revision": "09151f25e5446cda8adb3191032c8eec",
+    "url": "/clickygame/index.html"
+  }
+];
